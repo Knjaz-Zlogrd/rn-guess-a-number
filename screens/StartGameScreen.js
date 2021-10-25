3bdf4a035315a5b0863eb3608e1c2d745aebf674
Mihajlo <mulic92zr@gmail.com>
2021-10-25 18:24:16 +0200

    Did some configuring and styling of TextInput

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,22 +1,30 @@
 import React from "react";
-import {View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 import Card from "../components/Card";
+import Input from "../components/Input";
 import Colors from "../constants/colors";
 
 const StartGameScreen = props => {
   return (
-    <View style = {styles.screen}>
+    <View style={styles.screen}>
       <Text style={styles.title} >Start a New Game!</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
-        <TextInput/>
+        <Input 
+          style={styles.input} 
+          blurOnSubmit 
+          autoCapitalize='none' 
+          autoCorrect={false} 
+          keyboardType="number-pad" 
+          maxLength={2} 
+        />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Reset" onPress={() => {}} color={Colors.secondary} />
+            <Button title="Reset" onPress={() => { }} color={Colors.secondary} />
           </View>
           <View style={styles.button}>
-            <Button title="Confirm" onPress={() => {}} color={Colors.primary} />
+            <Button title="Confirm" onPress={() => { }} color={Colors.primary} />
           </View>
         </View>
       </Card>
@@ -46,7 +54,11 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15
   },
   button: {
-    width: 100   
+    width: 100
+  },
+  input: {
+    width: 50,
+    textAlign: 'center'
   }
 });
 
