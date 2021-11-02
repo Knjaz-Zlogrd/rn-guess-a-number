5dd47a42959da48489f0fc257e79892ed3a6c377
Mihajlo <mulic92zr@gmail.com>
2021-11-02 18:28:41 +0100

    Played around with Platform specific features

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,22 +1,36 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Touchable } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, TouchableNativeFeedback, Platform } from "react-native";
 
 import Colors from "../constants/colors";
 
 const MainButton = props => {
-  return <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
-    <View style={styles.button}>
-      <Text style={styles.buttonText}>{props.children}</Text>
+  let ButtonComponent = TouchableOpacity;
+
+  if (Platform.OS === 'android' && Platform.Version >= 21) {
+    ButtonComponent = TouchableNativeFeedback;
+  }
+
+  return (
+    <View style={styles.buttonContainer}>
+      <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>{props.children}</Text>
+        </View>
+      </ButtonComponent>
     </View>
-  </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    borderRadius: 25,
+    overflow: 'hidden'
+  },  
   button: {
     backgroundColor: Colors.primary,
     paddingVertical: 12,
     paddingHorizontal: 30,
-    borderRadius: 25 
+    borderRadius: 25
   },
   buttonText: {
     color: 'white',
